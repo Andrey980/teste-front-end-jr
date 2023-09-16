f79a5e8fcaf1ed2d43a0e740052924dc42e7a38a
Andrey980 <andreymcunhasilva@gmail.com>
2023-09-16 16:27:51 -0300

    Finalizado

--- teste-front-end/src/App.jsx
+++ teste-front-end/src/App.jsx
@@ -1,16 +1,9 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.scss'
 
 import Home from './pages/home/Home'
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  )
+  return <Home />
 }
 
 export default App
